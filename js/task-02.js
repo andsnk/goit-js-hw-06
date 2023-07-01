81f8c49e96469d1c20f5abf212092f7d5d93383e
andsnk <127045868+andsnk@users.noreply.github.com>
2023-07-01 13:15:24 +0300

    finish task-2

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,19 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const ingredientsList = document.querySelector("#ingredients");
+
+const ingredientElements = ingredients.map((ingredient) => {
+  const ingredientsItem = document.createElement("li");
+  ingredientsItem.textContent = ingredient;
+  ingredientsItem.classList.add("item");
+  return ingredientsItem;
+});
+
+ingredientsList.append(...ingredientElements);
