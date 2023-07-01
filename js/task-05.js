da68305835656760f72c4f603e2cf69e48a74212
andsnk <127045868+andsnk@users.noreply.github.com>
2023-07-01 22:47:28 +0300

    finish task-5

---
+++ js/task-05.js
@@ -0,0 +1,11 @@
+const textInput = document.querySelector("#name-input");
+const textOutput = document.querySelector("#name-output");
+const defaultName = textOutput.textContent;
+
+textInput.addEventListener("input", (event) => {
+  if (textInput.value === "") {
+    textOutput.textContent = defaultName;
+  } else {
+    textOutput.textContent = event.currentTarget.value;
+  }
+});
