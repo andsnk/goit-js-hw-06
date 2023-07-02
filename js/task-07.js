cf2a853b205f119a250939f6ec284333a0c6d460
andsnk <127045868+andsnk@users.noreply.github.com>
2023-07-02 16:19:26 +0300

    fix task-7

--- js/task-07.js
+++ js/task-07.js
@@ -6,10 +6,3 @@ function changeSize() {
   textElement.style.fontSize = inputFontSize.value + "px";
 }
 changeSize();
-// const fontSizeControl = document.querySelector("#font-size-control");
-// const textElement = document.querySelector("#text");
-
-// fontSizeControl.addEventListener("input", () => {
-//   const fontSize = fontSizeControl.value + "px";
-//   textElement.style.fontSize = fontSize;
-// });
