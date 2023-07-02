49ba502536fcaac572b8b95077be0b306a9495c8
andsnk <127045868+andsnk@users.noreply.github.com>
2023-07-02 21:05:10 +0300

    finish task-9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,12 @@
+const changeColor = document.querySelector(".change-color");
+const textColor = document.querySelector(".color");
+
+changeColor.addEventListener("click", () => {
+  const randomColor = getRandomHexColor();
+  document.body.style.backgroundColor = randomColor;
+  textColor.textContent = randomColor;
+});
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
