bbeeb8b542a9761e1ad5a17685ec42a393a3ae4e
andsnk <127045868+andsnk@users.noreply.github.com>
2023-07-02 18:36:16 +0300

    finish task-8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,19 @@
+const form = document.querySelector(".login-form");
+form.addEventListener("submit", formSubmit);
 
+function formSubmit(event) {
+  event.preventDefault();
+  const emailInput = form.elements.email;
+  const passwordInput = form.elements.password;
+  if (emailInput.value === "" || passwordInput.value === "") {
+    alert("Please, fill in all fields");
+    return;
+  } else {
+    const formData = {
+      email: emailInput.value,
+      password: passwordInput.value,
+    };
+    console.log(formData);
+    form.reset();
+  }
+}
