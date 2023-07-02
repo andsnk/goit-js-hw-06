9a568dca9a26ee1d822bf10616934321c67a2cb1
andsnk <127045868+andsnk@users.noreply.github.com>
2023-07-02 22:20:58 +0300

    finish task-10

--- js/task-10.js
+++ js/task-10.js
@@ -1,5 +1,38 @@
+const controls = document.querySelector("#controls");
+const input = controls.querySelector("input");
+const createBtn = controls.querySelector("[data-create]");
+const destroyBtn = controls.querySelector("[data-destroy]");
+const boxes = document.querySelector("#boxes");
+
+createBtn.addEventListener("click", () => {
+  const amount = Number(input.value);
+  createBoxes(amount);
+});
+
+destroyBtn.addEventListener("click", destroyBoxes);
+
+function createBoxes(amount) {
+  const fragment = document.createDocumentFragment();
+  const initialSize = 30;
+
+  for (let i = 0; i < amount; i++) {
+    const size = initialSize + i * 10;
+    const div = document.createElement("div");
+    div.style.width = `${size}px`;
+    div.style.height = `${size}px`;
+    div.style.backgroundColor = getRandomHexColor();
+    fragment.appendChild(div);
+  }
+
+  boxes.appendChild(fragment);
+}
+
+function destroyBoxes() {
+  boxes.innerHTML = "";
+}
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
-    .padStart(6, 0)}`;
+    .padStart(6, "0")}`;
 }
